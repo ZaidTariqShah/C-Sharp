6cb48e29defc13813d9483bc85703298a63e43c6
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-10-18 19:23:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prime_Number_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Prime = 11;
+            int num = 2;
+            int flag = 0;
+            while (num < Prime)
+            {
+                if (Prime % 2 == 0)
+                {
+                    flag = 1;
+                    break;
+                }
+                num++;
+            }
+            if (flag == 0)
+            {
+                Console.WriteLine("Number Is Prime");
+            }
+            else
+            {
+                Console.WriteLine("Number Is Not Prime");
+            }
+        }
+    }
+}
