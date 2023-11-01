861e72fef731c2011e0dca1a1b9a2078045f34c5
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-01 19:22:14 +0530

    Add files via upload

---
+++ PersonProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp12
+{
+    public class person
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+         public void SetField(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+        public void GetField()
+            {
+            Console.WriteLine($"{Name},{Age},{Address}");
+            }
+        
+    }
+    class PersonProgram
+    {
+        static void Main(string[] args)
+        {
+            person p = new person();
+            p.SetField("zaid",20,"ellahibagh");
+            p.GetField();
+        }
+    }
+}
