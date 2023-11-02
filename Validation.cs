06c68b3d78e25ccaf66100d7275e496f2cff49c5
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-02 18:44:09 +0530

    Add files via upload

---
+++ Validation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Validation
+{
+    public class Person
+    {
+        private string _name;
+
+        public string getName()
+        {
+            return _name;
+        }
+        public void setName(string Name)
+        {
+            _name = Name;
+            if(Name.Length <= 4 && Name[0] == 'Z' )
+            {
+                Console.WriteLine("Program Done Successfully");
+            }
+            else 
+            {
+                Console.WriteLine("Nope! Error Please Try Again");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Pupil = new Person();
+            Pupil.setName("Zaid");
+           string Naav =  Pupil.getName();
+            Console.WriteLine($"My Name is {Naav}");
+        }
+    }
+}
