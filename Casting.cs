00bea6d687135c16c1a4fe89ee4d842eab5c1d6a
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-02 22:50:25 +0530

    Add files via upload

---
+++ Casting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Casting
+{
+    class Casting
+    {
+        static void Main(string[] args)
+        {
+            int my = 10;                // Int to Double
+            double mi = my; 
+            Console.WriteLine(mi);
+            Console.WriteLine(my);
+
+            double mine = 9.78;
+            int mii = (int)mine;
+            Console.WriteLine(mii);
+            Console.WriteLine(mine);
+            double dob = 5.9;
+            int into = 10;
+            bool boo = true;
+            Console.WriteLine(Convert.ToString(into));          // Convert int To String
+            Console.WriteLine(Convert.ToDouble(into));          // Convert Int To Double
+            Console.WriteLine(Convert.ToInt32(dob));            // Convert Double To int
+            Console.WriteLine(Convert.ToString(boo));           // Convert Boolean To String
+
+        }
+    }
+}
