de5b6bac0b6ee5a6a353992fcde3b578db19b007
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-03 19:37:41 +0530

    Add files via upload

---
+++ Set_Get.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace value
+{
+    public class Board
+    {
+        private string _name;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value.Length > 0 && value.Length <= 4 && value[0] == 'Z')
+                {
+                    Console.WriteLine($"The Name is {value}");
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error! Try Again");
+                }
+            }
+        }
+    }
+
+    class Set_Get
+    {
+        static void Main(string[] args)
+        {
+            Board pen = new Board();
+            pen.Name = "Zara";    // Name should be less than 5 words
+        }
+    }
+}
