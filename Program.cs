aff7bf50536e3d1b3d71a458d2dd1e2876b179f0
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-14 21:12:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Virtual_And_Overriding
+{
+    public class Me
+    {
+        public virtual void more()
+        {
+            string name = "Zaid";
+            int age = 20;
+            int salary = 50000000;
+            Console.WriteLine("It Is Virtual");
+            Console.WriteLine($"Name: {name}, Age: {age}, Salary: {salary}");
+        }
+    }
+
+    public class Mine : Me
+    {
+        public  void more()
+        {
+            string name = "Zaid Tariq Shah";
+            int age = 20;
+            int salary = 50000000;
+            Console.WriteLine("It Is Override");
+            Console.WriteLine($"Name: {name}, Age: {age}, Salary: {salary}");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mine obj = new Mine();
+            obj.more();
+        }
+    }
+}
