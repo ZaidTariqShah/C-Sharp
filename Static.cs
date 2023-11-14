1c97c2f0c6c1ff6208f0a7afc28fdadde24f319c
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-14 22:04:46 +0530

    Add files via upload

---
+++ Static.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace C_
+{
+    public static class Me
+    {
+        public static void addtwo(int a, int b)
+        {
+            int z = a + b;
+            Console.WriteLine(z);
+        }
+        public static string name;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Me.addtwo(18,2);
+            Console.WriteLine(Me.name = "Zaid"); 
+        }
+    }
+}
