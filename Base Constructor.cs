1f52b1b176ac42f7921b61d9221592829b594e08
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-11-16 21:40:25 +0530

    Add files via upload

---
+++ Base Constructor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class TwoD
+{
+    public int length;
+    public int breadth;
+
+    public TwoD(int x = 5, int y = 4)
+    {
+        this.length = x;
+        this.breadth = y;
+    }
+
+    public int GetResults()
+    {
+        return this.length * this.breadth;
+    }
+}
+
+public class ThreeD : TwoD
+{
+    public int height;
+
+    public ThreeD(int a, int b, int c) : base(a, b)
+    {
+        this.height = c;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ThreeD obj = new ThreeD(2, 3, 4);
+        Console.WriteLine($"Length: {obj.length}");
+        Console.WriteLine($"Breadth: {obj.breadth}");
+        Console.WriteLine($"Height: {obj.height}");
+        int result = obj.GetResults();
+        Console.WriteLine($"Result: {result}");
+    }
+}
